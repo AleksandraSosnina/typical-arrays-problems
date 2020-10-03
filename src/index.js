c14f848809ecd1697c11c5b03b30c38e3d6c1cb7
Sosnina Sasha <aleksandra.sosnina@unitbean.com>
2020-10-03 23:09:09 +0400

    feat: implemented max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum / array.length;
 }
